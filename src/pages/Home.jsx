55c834d383bd0bd667fd8e64f1350f37e6138c9c
Ismael Barea Insua <80041279+quantosh@users.noreply.github.com>
2023-02-17 10:00:08 +0100

    Cambios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,15 @@ import { Outlet } from 'react-router-dom'
 import Topbar from '../components/topbar'
 import Sidebar from '../components/sidebar'
 import Footer from '../components/footer'
+import Spinner from '../components/spinner'
 
 const style = {
   mainContainer: 'text-black backdrop-blur-sm bg-white/40 border border-white/40 flex rounded-x shadow-xl w-full h-full',
-  topContainer: 'mb-1 float-right w-full border-b border-gray/40 bg-white',
   sideContainer: 'h-full shadow-xl border-r border-gray/40 bg-white',
+  contentContainer: 'block w-full',
+  topContainer: 'mb-1 float-right w-full border-b border-gray/40 bg-white',
   appContainer: 'bg-white',
-  footerContainer: 'bg-white border-t border-gray-200',
-  contentContainer: 'block w-full'
+  footerContainer: 'bg-white border-t border-gray-200'
 }
 
 const Home = () => {
@@ -26,15 +27,14 @@ const Home = () => {
           </div>
           <div className={style.appContainer}>
             <Outlet />
+            <Spinner />
           </div>
           <div className={style.footerContainer}>
             <Footer />
           </div>
         </div>
         {/* This side if I want to manage a right panel */}
-        <div>
-          
-        </div>
+        <div />
       </div>
     </>
   )
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -3,7 +3,8 @@ import { Outlet } from 'react-router-dom'
 import Notfound from '../components/notfound'
 
 const style = {
-  mainContainer: 'text-black backdrop-blur-sm bg-white/40 border border-white/40 flex rounded-x shadow-xl w-full h-full',
+  mainContainer:
+    'text-black backdrop-blur-sm bg-white/40 border border-white/40 flex rounded-x shadow-xl w-full h-full',
   topContainer: 'mb-1 float-right w-full border-b border-gray/40 bg-white',
   sideContainer: 'h-full shadow-xl border-r border-gray/40 bg-white',
   appContainer: 'bg-white',
@@ -11,14 +12,12 @@ const style = {
   contentContainer: 'block w-full'
 }
 
-const NotFoundPage = () => {
-  return (
-    <div className='h-screen w-screen bg-gray-100 flex items-center'>
-      <div className='container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700'>
+const NotFoundPage = () => (
+  <div className='h-screen w-screen bg-gray-100 flex items-center'>
+    <div className='container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700'>
       <Notfound />
-      </div>
     </div>
-  )
-}
+  </div>
+)
 
 export default NotFoundPage
